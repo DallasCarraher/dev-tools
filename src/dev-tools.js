e8ca882c526931d3ae53b146791571f9dc205367
DallasCarraher <dallas.carraher@gmail.com>
2021-04-27 20:24:54 -0700

    we have something on the screen 👍

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -16,5 +16,28 @@ function install() {
       '__bookshelf_devtools_tab_index__',
       0
     )
+
+    const show = persist || hovering
+    const toggleShow = () => setPersist(v => !v)
+    React.useEffect(() => {
+      function updateHoverState(event) {
+        setHovering(rootRef.current?.contains(event.target) ?? false)
+      }
+      document.body.addEventListener('mousemove', updateHoverState)
+      return () =>
+        document.body.removeEventListener('mousemove', updateHoverState)
+    }, [])
+
+    return (
+      <div>
+        <p>something</p>
+      </div>
+    )
   }
+
+  const devToolsRoot = document.createElement('div')
+  document.body.appendChild(devToolsRoot)
+  ReactDOM.render(<DevTools />, devToolsRoot)
 }
+
+export { install }
