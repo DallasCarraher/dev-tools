30461a5afd9ad4922279f3c43c18d98b47772cb7
DallasCarraher <dallas.carraher@gmail.com>
2021-04-28 01:42:35 -0700

    start of dev tools !

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -1,37 +1,56 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { FaTools } from 'react-icons/fa'
+import { Box, Button, Text } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
+import Decorator from './Decorator'
 import useLocalStorageState from './hooks/useLocalStorageState'
+
+const ToolBox = styled(Box)`
+  position: fixed;
+  bottom: ${({ bottom }) => bottom}px;
+  /* opacity: 80%; */
+  background-color: rgba(255, 255, 255, 0.08);
+  /* box-sizing: content-box; */
+  transition: all 0.5s cubic-bezier(0, 1, 0.5, 1);
+`
+
+const ToggleBox = styled(Button)`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  padding: 10px 20px;
+  border-radius: 0px;
+  margin-top: -40px;
+  overflow: hidden;
+  border-top-right-radius: 10px;
+`
 
 function install() {
   function DevTools() {
     const rootRef = React.useRef()
-    const [hovering, setHovering] = React.useState(false)
     const [persist, setPersist] = useLocalStorageState(
-      '__bookshelf_devtools_persist__',
+      '__devtools_persist__',
       false
     )
-    const [tabIndex, setTabIndex] = useLocalStorageState(
-      '__bookshelf_devtools_tab_index__',
-      0
-    )
 
-    const show = persist || hovering
     const toggleShow = () => setPersist(v => !v)
-    React.useEffect(() => {
-      function updateHoverState(event) {
-        setHovering(rootRef.current?.contains(event.target) ?? false)
-      }
-      document.body.addEventListener('mousemove', updateHoverState)
-      return () =>
-        document.body.removeEventListener('mousemove', updateHoverState)
-    }, [])
+
+    const style = persist ? '0' : '-200'
+    const text = persist ? 'close' : 'dev-tools'
 
     return (
-      <div>
-        <p>something</p>
-      </div>
+      <Decorator>
+        <ToolBox w="100%" h="200px" bottom={style}>
+          <ToggleBox leftIcon={<FaTools />} onClick={toggleShow}>
+            {text}
+          </ToggleBox>
+          <Text fontSize="4xl" p="2">
+            Dallas's Dev Tools
+          </Text>
+        </ToolBox>
+      </Decorator>
     )
   }
 
