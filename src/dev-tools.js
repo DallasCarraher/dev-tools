cce3f92273ba3be61aa8642e48e84afd71478280
DallasCarraher <dallas.carraher@gmail.com>
2021-04-27 20:19:18 -0700

    no semis

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -1,18 +1,20 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { FaTools } from 'react-icons/fa';
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { FaTools } from 'react-icons/fa'
+
+import useLocalStorageState from './hooks/useLocalStorageState'
 
 function install() {
   function DevTools() {
-    const rootRef = React.useRef();
-    const [hovering, setHovering] = React.useState(false);
+    const rootRef = React.useRef()
+    const [hovering, setHovering] = React.useState(false)
     const [persist, setPersist] = useLocalStorageState(
       '__bookshelf_devtools_persist__',
       false
-    );
+    )
     const [tabIndex, setTabIndex] = useLocalStorageState(
       '__bookshelf_devtools_tab_index__',
       0
-    );
+    )
   }
 }
